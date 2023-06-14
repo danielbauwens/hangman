81a4d1c625debedbb271cac565395b648cdd9411
danielbauwens <69080058+danielbauwens@users.noreply.github.com>
2023-06-14 12:33:20 +0200

    Midday commit
    Updated with working main file, currently bugfixing

--- utils/game.py
+++ utils/game.py
@@ -2,17 +2,18 @@ import random
 # Importing the random library to be able to randomize the guessing word.
 
 
-class hangman:
+class Hangman:
     def __init__(self):
         # List of attributes
         self.possible_words = ['becode', 'learning', 'mathematics', 'sessions']
         self.word_to_find = []
         self.lives = 5
         self.correctly_guessed_letters = []
-        self.wrongly_guessed_letters = {}
+        self.wrong = []
+        self.wrongly_guessed_letters = set(self.wrong)
         self.turn_count = 0
         self.error_count = len(self.wrongly_guessed_letters)
-        self.guessed_letters = guess
+
         ''' Picks a random word from the list and breaks it down into single letters.
         The correct list gets updated to display "_" for every letter in the word'''
         self.word = random.choice(self.possible_words)
@@ -22,18 +23,22 @@ class hangman:
 
     def play(self):
         '''Method to ask user for their guess.
-        Putting the guess into the correct category.'''
-        guess = str(input("Please enter a letter to guess:"))
-        if len(guess) >= 2:
-            hangman.play()
-            print("Cannot be more than one letter. Try again.")
+        Putting the guess into the correct category.
+        Lastly increasing the turn counter by 1'''
+        print(f"Lives remaining: {self.lives}")
+        print(f"It's not these words: {self.wrongly_guessed_letters}")
+        print(f"This is the word you're looking for: {self.correctly_guessed_letters}")
+        guess = input("Please enter a letter to guess:")
+        if len(guess) >= 2 or guess != str:
+            print("Has to be a letter and cannot be more than one letter. Try again.")
+            self.play()
         else: 
-            
             for i in self.word:
                 if guess.casefold() != i.casefold():
-                    self.wrongly_guessed_letters + i 
+                    self.wrong.append(i)
                 else:
                     self.correctly_guessed_letters.append(i)
+            self.turn_count += 1
 
     def game_over(self):
         self.lives = 0
@@ -43,8 +48,8 @@ class hangman:
         print(f"You found the word {self.word.upper()} in {self.turn_count} turns with {self.error_count} errors!")
 
     def start_game(self):
-        while lives > 0:
-            hangman.play()
+        while self.lives > 0:
+            self.play()
         else:
             self.game_over()
         if self.correctly_guessed_letters is self.word_to_find:
