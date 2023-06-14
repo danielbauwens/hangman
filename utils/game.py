4ba39104a1dae2d17acec1f7a55e037dc3ad757e
danielbauwens <69080058+danielbauwens@users.noreply.github.com>
2023-06-14 14:56:58 +0200

    test

--- utils/game.py
+++ utils/game.py
@@ -5,21 +5,20 @@ class Hangman:
     def __init__(self):
         # List of attributes
         self.possible_words = ['becode', 'learning', 'mathematics', 'sessions']
+        ''' Picks a random word from the list and breaks it down into single letters.
+        The correct list gets updated to display "_" for every letter in the word'''
         self.word = random.choice(self.possible_words)
         self.word_to_find = []
-        self.lives = 5
+        for l in self.word:
+            self.word_to_find.append(l.lower())
+
+        self.lives = 15
         self.correctly_guessed_letters = list("_"* len(self.word)) 
         self.wrong = []
-        set(self.wrong)
+        self.wrong = list(dict.fromkeys(self.wrong))
         self.wrongly_guessed_letters = self.wrong
         self.turn_count = 0
         self.error_count = len(self.wrongly_guessed_letters)
-
-        ''' Picks a random word from the list and breaks it down into single letters.
-        The correct list gets updated to display "_" for every letter in the word'''
-        
-        for l in self.word:
-            self.word_to_find.append(l)
 
     def play(self):
         '''Method to ask user for their guess.
@@ -36,25 +35,27 @@ class Hangman:
             for position, letter in enumerate(self.word):
                 if guess.casefold() != letter.casefold():
                     self.wrong.append(guess)
+                    self.lives -= 1
                 else:
                     self.correctly_guessed_letters[position] = letter
             self.turn_count += 1
 
     def game_over(self):
-        
-        self.lives = 0
         print("Game over...")
 
     def well_played(self):
         print(f"You found the word {self.word.upper()} in {self.turn_count} turns with {self.error_count} errors!")
 
     def start_game(self):
-        while self.lives > 0:
-            self.play()
+        while self.lives != 0:
+            if self.correctly_guessed_letters == self.word_to_find:
+                self.well_played()
+            else:
+                while self.lives > 0:
+                    self.play()
         else:
             self.game_over()
-        if self.correctly_guessed_letters is self.word_to_find:
-            self.well_played()
+        
 
 
 
